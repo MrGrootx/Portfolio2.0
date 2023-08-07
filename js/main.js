79c379abbf7eb62fa84e84861f3853bd3f3734bf
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2023-08-07 13:44:01 +0530

    Portfolio2.0

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+const src = ScrollReveal ({
+    distance: '35px',
+    duration: 2400,
+    reset: true
+});
+
+src.reveal('.logo', {delay:210, origin: 'left'})
+src.reveal('.navlist', {delay:310, origin: 'top'})
+src.reveal('.top-btnn', {delay:410, origin: 'right'})
+
+src.reveal('.main-content', {delay:510, origin: 'top'})
+src.reveal('.main-content h1', {delay:610, origin: 'left'})
